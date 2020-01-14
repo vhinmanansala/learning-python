6216a1d176fe6a8bc0efcb478019ef4d5e20f34f
Alvin Manansala <aomanansala@gmail.com>
2020-01-14 22:38:11 +1030

    class variables

--- oop.py
+++ oop.py
@@ -1,15 +1,29 @@
 class Employee:
+
+	num_of_employees = 0
+	raise_amount = 1.04
+
 	def __init__(self, first, last, pay):
 		self.first = first
 		self.last = last
 		self.pay = pay
 		self.email = first + '.' + last + '@company.com'
 
+		Employee.num_of_employees += 1
+
 	def fullname(self):
 		return self.first + ' ' + self.last
+
+	def apply_raise(self):
+		self.pay = int(self.pay * self.raise_amount)
 
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'User', 60000)
 
 print(emp_1.fullname())
 print(emp_2.fullname())
+
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
+print(Employee.num_of_employees)
