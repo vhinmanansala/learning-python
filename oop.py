5d6d2a11d80de5a98b37d27aa573234a15451a62
Alvin Manansala <aomanansala@gmail.com>
2020-01-14 23:11:29 +1030

    class and static method

--- oop.py
+++ oop.py
@@ -17,6 +17,17 @@ class Employee:
 	def apply_raise(self):
 		self.pay = int(self.pay * self.raise_amount)
 
+	@classmethod
+	def from_string(cls, emp_str):
+		first, last, pay = emp_str.split('-')
+		return cls(first, last, pay)
+
+	@staticmethod
+	def is_workday(day):
+		if day.weekday() == 5 or day.weekday() == 6:
+			return False
+		return True
+
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'User', 60000)
 
@@ -27,3 +38,12 @@ print(Employee.raise_amount)
 print(emp_1.raise_amount)
 print(emp_2.raise_amount)
 print(Employee.num_of_employees)
+
+emp_str_1 = 'John-Doe-70000'
+new_emp_1 = Employee.from_string(emp_str_1)
+print(new_emp_1.email)
+print(new_emp_1.pay)
+
+import datetime
+my_date = datetime.date(2016, 7, 10)
+print(Employee.is_workday(my_date))
