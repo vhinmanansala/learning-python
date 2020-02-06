90d73f376d86433b8bc4810eac97b6424e10d9b8
Alvin Manansala <aomanansala@gmail.com>
2020-02-06 22:44:13 +1030

    property decorators - getters, setters, and deleters

--- oop.py
+++ oop.py
@@ -1,61 +1,36 @@
 class Employee:
 
-	num_of_employees = 0
-	raise_amount = 1.04
-
-	def __init__(self, first, last, pay):
+	def __init__(self, first, last):
 		self.first = first
 		self.last = last
-		self.pay = pay
-		self.email = first + '.' + last + '@company.com'
+	
+	@property
+	def email(self):
+		return self.first + '.' + self.last + '@company.com'
 
+	@property
 	def fullname(self):
 		return self.first + ' ' + self.last
 
-	def apply_raise(self):
-		self.pay = int(self.pay * self.raise_amount)
+	@fullname.setter
+	def fullname(self, name):
+		first, last = name.split(' ')
+		self.first = first
+		self.last = last
 
-class Developer(Employee):
-	raise_amount = 1.10
+	@fullname.deleter
+	def fullname(self):
+		print('Delete name')
+		self.first = None
+		self.last = None
 
-	def __init__(self, first, last, pay, prog_lang):
-		super().__init__(first, last, pay)
-		self.prog_lang = prog_lang
 
-class Manager(Employee):
-	def __init__(self, first, last, pay, employees = None):
-		super().__init__(first, last, pay)
+emp_1 = Employee('John', 'Smith')
+emp_1.first = 'Jim'
+emp_1.fullname = 'Corey Schafer'
 
-		if employees is None:
-			self.employees = []
-		else:
-			self.employees = employees
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
 
-	def add_emp(self, emp):
-		if emp not in self.employees:
-			self.employees.append(emp)
-
-	def remove_emp(self, emp):
-		if emp in self.employees:
-			self.employees.remove(emp)
-
-	def print_emps(self):
-		for emp in self.employees:
-			print('-->', emp.fullname())
-
-dev_1 = Developer('Corey', 'Schafer', 50000, 'python')
-dev_2 = Developer('Test', 'User', 60000, 'java')
-
-#print(dev_1.pay)
-#dev_1.apply_raise()
-#print(dev_1.pay)
-
-#print(dev_1.email)
-#print(dev_1.prog_lang)
-
-mgr_1 = Manager('Sue', 'Smith', 9000, [dev_1])
-
-print(mgr_1.email)
-mgr_1.add_emp(dev_2)
-mgr_1.remove_emp(dev_1)
-mgr_1.print_emps()
+del emp_1.fullname
