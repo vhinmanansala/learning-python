b2b5a81d9f96e047d4d73dc8bd46e1dbe6709918
Alvin Manansala <aomanansala@gmail.com>
2020-02-06 21:23:27 +1030

    inheritance and sub classes

--- oop.py
+++ oop.py
@@ -9,41 +9,53 @@ class Employee:
 		self.pay = pay
 		self.email = first + '.' + last + '@company.com'
 
-		Employee.num_of_employees += 1
-
 	def fullname(self):
 		return self.first + ' ' + self.last
 
 	def apply_raise(self):
 		self.pay = int(self.pay * self.raise_amount)
 
-	@classmethod
-	def from_string(cls, emp_str):
-		first, last, pay = emp_str.split('-')
-		return cls(first, last, pay)
+class Developer(Employee):
+	raise_amount = 1.10
 
-	@staticmethod
-	def is_workday(day):
-		if day.weekday() == 5 or day.weekday() == 6:
-			return False
-		return True
+	def __init__(self, first, last, pay, prog_lang):
+		super().__init__(first, last, pay)
+		self.prog_lang = prog_lang
 
-emp_1 = Employee('Corey', 'Schafer', 50000)
-emp_2 = Employee('Test', 'User', 60000)
+class Manager(Employee):
+	def __init__(self, first, last, pay, employees = None):
+		super().__init__(first, last, pay)
 
-print(emp_1.fullname())
-print(emp_2.fullname())
+		if employees is None:
+			self.employees = []
+		else:
+			self.employees = employees
 
-print(Employee.raise_amount)
-print(emp_1.raise_amount)
-print(emp_2.raise_amount)
-print(Employee.num_of_employees)
+	def add_emp(self, emp):
+		if emp not in self.employees:
+			self.employees.append(emp)
 
-emp_str_1 = 'John-Doe-70000'
-new_emp_1 = Employee.from_string(emp_str_1)
-print(new_emp_1.email)
-print(new_emp_1.pay)
+	def remove_emp(self, emp):
+		if emp in self.employees:
+			self.employees.remove(emp)
 
-import datetime
-my_date = datetime.date(2016, 7, 10)
-print(Employee.is_workday(my_date))
+	def print_emps(self):
+		for emp in self.employees:
+			print('-->', emp.fullname())
+
+dev_1 = Developer('Corey', 'Schafer', 50000, 'python')
+dev_2 = Developer('Test', 'User', 60000, 'java')
+
+#print(dev_1.pay)
+#dev_1.apply_raise()
+#print(dev_1.pay)
+
+#print(dev_1.email)
+#print(dev_1.prog_lang)
+
+mgr_1 = Manager('Sue', 'Smith', 9000, [dev_1])
+
+print(mgr_1.email)
+mgr_1.add_emp(dev_2)
+mgr_1.remove_emp(dev_1)
+mgr_1.print_emps()
